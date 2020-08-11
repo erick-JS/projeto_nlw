d2f44c08733d93ca3815d9315f757f69334be53b
erick-JS <erick.silva64@fatec.sp.gov.br>
2020-08-10 22:02:06 -0300

    criando pasta site

---
+++ site/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Routes from './pages/routes';
+
+//JSX - JavaScript + XML
+//TSX - TypeScript + XML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
