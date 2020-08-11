310eee004a83b2b2b9aa6e9d91740003809db55f
erick-JS <erick.silva64@fatec.sp.gov.br>
2020-08-10 22:08:16 -0300

    criando pasta app

---
+++ app/App.tsx
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { AppLoading } from 'expo'
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+import AppStack from './src/routes/AppStack';
+
+//JSX - JavaScript + XML
+//TSX - TypeScript + XML
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <AppStack/>
+
+        <StatusBar style="light" />
+      </>
+    );
+  }
+}
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 32,
+    color: '#FFF',
+    fontWeight: 'bold',
+    transform: [
+      { rotateZ: '30deg' }
+    ]
+  },
+});*/
